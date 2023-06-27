d0d58aff7884be5803fa488ffad80c1706d07430
Aaron Otto <122137616+ottoaara@users.noreply.github.com>
2023-06-26 20:55:15 -0500

    Update crowdfunding_db_schema.sql

--- Starter_Files/Resources/crowdfunding_db_schema.sql
+++ Starter_Files/Resources/crowdfunding_db_schema.sql
@@ -3,20 +3,20 @@
 -- Database: crowdfunding_db
 
 -- DROP DATABASE IF EXISTS crowdfunding_db;
+-- if no DB created remove comment below and run create DB statement. 
+--CREATE DATABASE crowdfunding_db
+  --  WITH
+    --OWNER = postgres
+    --ENCODING = 'UTF8'
+    --LC_COLLATE = 'C'
+    --LC_CTYPE = 'C'
+    --TABLESPACE = pg_default
+    --CONNECTION LIMIT = -1
+    --IS_TEMPLATE = False;
 
-CREATE DATABASE crowdfunding_db
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'C'
-    LC_CTYPE = 'C'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
-
-COMMENT ON DATABASE crowdfunding_db
-    IS 'project 2
-';
+--COMMENT ON DATABASE crowdfunding_db
+  --  IS 'project 2
+--';
 
 
 CREATE TABLE "campaign" (
